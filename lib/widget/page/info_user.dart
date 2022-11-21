b425f9c916da1281002a0f12b3c1edddec9cac65
Erwan Auer <erwan.auer@epita.fr>
2022-11-22 00:46:55 +0100

    [WIDGET] : add first implementation of user

--- lib/widget/page/info_user.dart
+++ lib/widget/page/info_user.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
 import 'package:isar/isar.dart';
+import 'package:get_it/get_it.dart';
 import 'package:styled_widget/styled_widget.dart';
+import 'package:provider/provider.dart';
+
+import 'package:contacts/model/user.dart';
+import 'package:contacts/provider/user_provider.dart';
 
 class InfoUserPage extends StatelessWidget {
-  InfoUserPage({super.key, required this.id});
+  InfoUserPage({super.key, required this.id}) {
+    init();
+  }
+
+  void init() async {
+    if (id != null) {
+      _user = await GetIt.instance<Isar>().users.get(id!) ?? User();
+    } else {
+      _user = User();
+    }
+  }
 
   final Id? id;
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  late final User _user;
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +34,10 @@ class InfoUserPage extends StatelessWidget {
               onPressed: () {
                 if (_formKey.currentState?.validate() ?? false) {
                   // ajout ou modification de l'utilisateur
+                  if (id == null) {
+                    context.read<UserProvider>().addUser(_user);
+                    Navigator.of(context).pop();
+                  }
                 }
               },
               tooltip: 'Validate',
@@ -26,22 +46,39 @@ class InfoUserPage extends StatelessWidget {
       ),
       body: _InfoUserForm(
         formKey: _formKey,
+        user: _user,
       ),
     );
   }
 }
 
 class _InfoUserForm extends StatefulWidget {
-  const _InfoUserForm({required this.formKey});
+  const _InfoUserForm({required this.formKey, required this.user});
 
   final GlobalKey<FormState> formKey;
+  final User user;
 
   @override
   State<_InfoUserForm> createState() => _InfoUserState();
 }
 
 class _InfoUserState extends State<_InfoUserForm> {
-  _InfoUserState();
+  final TextEditingController _controllerName = TextEditingController();
+  final TextEditingController _controllerSurname = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+    _controllerName.addListener(() => widget.user.name = _controllerName.text);
+    _controllerSurname
+        .addListener(() => widget.user.surname = _controllerSurname.text);
+  }
+
+  @override
+  void dispose() {
+    _controllerName.dispose();
+    _controllerSurname.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -51,26 +88,33 @@ class _InfoUserState extends State<_InfoUserForm> {
         mainAxisAlignment: MainAxisAlignment.start,
         children: <Widget>[
           TextFormField(
-              decoration: const InputDecoration(
-                hintText: 'First Name',
-                labelText: 'First Name',
-              ),
-              validator: (String? value) {
-                if (value == null || value.isEmpty) {
-                  return 'A contact must have a first name';
-                }
-                return null;
-              }),
+            initialValue: widget.user.name,
+            decoration: const InputDecoration(
+              hintText: 'First Name',
+              labelText: 'First Name',
+            ),
+            validator: (String? value) {
+              if (value == null || value.isEmpty) {
+                return 'A contact must have a first name';
+              }
+              return null;
+            },
+            controller: _controllerName,
+          ),
           TextFormField(
+            initialValue: widget.user.surname,
             decoration: const InputDecoration(
               hintText: 'Last Name',
               labelText: 'Last Name',
             ),
+            controller: _controllerSurname,
           ),
           InputDatePickerFormField(
+            initialDate: widget.user.birthDate,
             firstDate: DateTime.utc(1900, 1, 1),
             lastDate: DateTime.now(),
-            fieldLabelText: 'Birthday',
+            fieldLabelText: 'Birthdate',
+            onDateSubmitted: (value) => widget.user.birthDate = value,
           )
         ],
       ),
